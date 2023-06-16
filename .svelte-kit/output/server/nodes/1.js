0f26fd5a4b9160ca06d808c8eaac4c3d42604396
Martin M <48408674+MartinTintin3@users.noreply.github.com>
2023-06-16 12:09:16 -0400

    Delete .svelte-kit directory

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.551ab65c.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.d1470d0f.js"];
-export const stylesheets = [];
-export const fonts = [];
