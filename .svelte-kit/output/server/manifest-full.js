40a6ae15bcae5e4d52d712211082ccf70f56b803
MartinTintin3 <maroymart@gmail.com>
2023-06-16 09:45:46 -0400

    Better

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -4,7 +4,7 @@ export const manifest = {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.c7bf8d0e.js","app":"_app/immutable/entry/app.b0f58f8b.js","imports":["_app/immutable/entry/start.c7bf8d0e.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.e52f2d73.js","_app/immutable/entry/app.b0f58f8b.js","_app/immutable/chunks/index.6dba6488.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.6164a068.js","app":"_app/immutable/entry/app.4f3c173d.js","imports":["_app/immutable/entry/start.6164a068.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.d1470d0f.js","_app/immutable/entry/app.4f3c173d.js","_app/immutable/chunks/index.6dba6488.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			() => import('./nodes/0.js'),
 			() => import('./nodes/1.js'),
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -2,6 +2,6 @@
 
 export const index = 1;
 export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.6bb1d5ad.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.e52f2d73.js"];
+export const imports = ["_app/immutable/nodes/1.551ab65c.js","_app/immutable/chunks/index.6dba6488.js","_app/immutable/chunks/singletons.d1470d0f.js"];
 export const stylesheets = [];
 export const fonts = [];
