50d7f91acb47549867bdbf20c6d81b5d2c8a1fc5
MartinTintin3 <maroymart@gmail.com>
2023-06-16 11:38:21 -0400

    e

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Lightning Calculator</h1>
+<h2>Calculate the distance to ligtning</h2>
