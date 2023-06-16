84982a7cee25f66c9c84e99b23b42ecb2fcff270
MartinTintin3 <maroymart@gmail.com>
2023-06-16 12:07:41 -0400

    Done (no styles yet)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+	let time = 10;
+</script>
+
 <h1>Lightning Calculator</h1>
 <h2>Calculate the distance to ligtning</h2>
+
+<label for="seconds">Seconds in between light and sound:</label>
+<input bind:value={time} type="number" name="seconds">
+<p id="result">{Math.round(time * 343 / 1000 * 10) / 10} kilometers away, {Math.round(time * 343 / 1000 / 1.609 * 10) / 10} miles away</p>
+
+<style>
+	* {
+		text-align: center;
+		margin: auto;
+	}
+</style>
